14fe2212028be4ccc0a3eabf7e86f18e253040fa
abiudesu <86725765+abiudesu@users.noreply.github.com>
2023-05-05 12:55:09 +0800

    Create README.md

--- digital pet/dp_main.c
+++ digital pet/dp_main.c
@@ -5,88 +5,163 @@
 #include<SDL_ttf.h>
 #include<SDL_mixer.h>
 
-void QUIT();
-void LOADMain();
-void LOADTitle();
-void LOADRoom();
-void LOADRubbish();
-void DrawRubbish();
-void RectShapeonMouseEvent(SDL_Event* event, int i);
+#define NUM_TARGETS 3
 
-int scene;
+SDL_Window *Window = NULL;
+SDL_Renderer *Renderer = NULL;
 
-int rtype[2];
-int rDragenable = 0;
-SDL_FPoint rDragStartPoint;
-SDL_FPoint rDragStartMousePoint;
-SDL_FPoint currentMousePoint;
-SDL_FRect Rubbishrect[2];
+typedef struct {
+	SDL_Rect dest;
+	int is_dragging;
+	int tartype;
+	int disappear;
+}Targets;
+//disappear为1需要消失0不需要
+//丢完垃圾进入下个场景1为不进0为进,count用于计算是否每个垃圾都消失
+int Enternextscene;
+int count;
 
-SDL_Window* Window = NULL;
-SDL_Renderer* Renderer = NULL;
+Targets targets[NUM_TARGETS];
 
-SDL_Surface* MainSurface = NULL;
-SDL_Texture* MainTexture = NULL;
-SDL_Rect MainRect;
+void event_loop();
+void event_loop2();
 
-SDL_Surface* StartSurface = NULL;
-SDL_Texture* StartTexture = NULL;
-SDL_Rect StartRect;
+SDL_Surface* Room_Surface = NULL;
+SDL_Texture* Room_Texture = NULL;
+SDL_Rect Room_Rect;
+SDL_Rect RB_Rect;
+SDL_Surface* Banana_Surface = NULL;
+SDL_Texture* Banana_Texture = NULL;
+SDL_Surface* Paper_Surface = NULL;
+SDL_Texture* Paper_Texture = NULL;
 
-SDL_Surface* Rubbish_Surface = NULL;
-SDL_Texture* Rubbish_Texture = NULL;
-SDL_Rect Rubbish_Rect;
-
-TTF_Font* TitleFont = NULL;
-SDL_Surface* TitleSurface = NULL;
-SDL_Texture* TitleTexture = NULL;
-SDL_Rect TitleRect;
-SDL_Color Titlecolor = { 109,56,50,255 };
-
+SDL_Surface* BG_Surface = NULL;
+SDL_Texture* BG_Texture = NULL;
+SDL_Surface* Rabbit_Surface = NULL;
+SDL_Texture* Rabbit_Texture = NULL;
+SDL_Surface* Carrot_Surface = NULL;
+SDL_Texture* Carrot_Texture = NULL;
+SDL_Rect src1;
+SDL_Rect src2;
+SDL_Rect put1;
+SDL_Rect put2;
+int walkspeed;
+int bg_x;
 
 int SDL_main(int argc, char* argv[]) {
 	SDL_Init(SDL_INIT_VIDEO);
 	TTF_Init();
 
-	SDL_Event MainEvent;
 	Window = SDL_CreateWindow("Digital Pet", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 710, 710, SDL_WINDOW_SHOWN);
 	Renderer = SDL_CreateRenderer(Window, -1, SDL_RENDERER_ACCELERATED);
-	int scene = 1;
+	Enternextscene = 1;
 
-	LOADMain();
-	LOADTitle();
-	//SDL_Delay(5000);
+	//写入不同垃圾的位置
+	srand(time(NULL));
+	int tar;
+	for (tar = 0; tar < NUM_TARGETS; tar++) {
+		targets[tar].dest.x = rand() % 401;
+		targets[tar].dest.y= 500 + rand() % 181;
+		targets[tar].dest.w = 60;
+		targets[tar].dest.h = 60;
+		targets[tar].is_dragging = 0;
+		targets[tar].tartype = rand() % 2 + 1;
+		targets[tar].disappear = 0;
+	}
 
-	SDL_FreeSurface(MainSurface);
-	SDL_FreeSurface(TitleSurface);
-	SDL_DestroyTexture(MainTexture);
-	SDL_DestroyTexture(TitleTexture);
-	TTF_CloseFont(TitleFont);
+	//加载垃圾和背景和垃圾桶
+	Room_Rect.x = 0;
+	Room_Rect.y = 0;
+	Room_Rect.w = 710;
+	Room_Rect.h = 710;
+	RB_Rect.x = 580;
+	RB_Rect.y = 570;
+	RB_Rect.w = 115;
+	RB_Rect.h = 140;
+	Room_Surface = IMG_Load("room1.png");
+	Room_Texture = SDL_CreateTextureFromSurface(Renderer, Room_Surface);
+	Banana_Surface = IMG_Load("banana.png");
+	Banana_Texture = SDL_CreateTextureFromSurface(Renderer, Banana_Surface);
+	Paper_Surface = IMG_Load("paper.png");
+	Paper_Texture = SDL_CreateTextureFromSurface(Renderer, Paper_Surface);
+	event_loop();
+	//释放内存
+	SDL_FreeSurface(Room_Surface);
+	SDL_FreeSurface(Banana_Surface);
+	SDL_FreeSurface(Paper_Surface);
+	SDL_DestroyTexture(Room_Texture);
+	SDL_DestroyTexture(Banana_Texture);
+	SDL_DestroyTexture(Paper_Texture);
 	SDL_RenderClear(Renderer);
-	LOADRoom();
-	LOADRubbish();
-	SDL_RenderPresent(Renderer);
+	//进入场景2
+	//加载资源,人物，背景，萝卜
+	BG_Surface = IMG_Load("grass.png");
+	BG_Texture = SDL_CreateTextureFromSurface(Renderer, BG_Surface);
+	Rabbit_Surface = IMG_Load("walk1.png");
+	Rabbit_Texture = SDL_CreateTextureFromSurface(Renderer, Rabbit_Surface);
+	Carrot_Surface = IMG_Load("carrot.png");
+	Carrot_Texture = SDL_CreateTextureFromSurface(Renderer, Carrot_Surface);
+	//加载萝卜
+	event_loop2();
+	
+	
 
-	while (1) {
+	return 0;
+}
+
+void event_loop() {
+	while (Enternextscene) {
+		SDL_Event MainEvent;
 		long Begin = SDL_GetTicks();
-		if (scene == 1) {
-			while (SDL_PollEvent(&MainEvent)) {
-				switch (MainEvent.type) {
-					int i;
-				case SDL_MOUSEMOTION:
-				case SDL_MOUSEBUTTONDOWN:
-				case SDL_MOUSEBUTTONUP:
-					for (i = 0; i < 2; i++) {
-						RectShapeonMouseEvent(&MainEvent,i);
+		if (SDL_PollEvent(&MainEvent)) {
+			if (MainEvent.key.keysym.sym == SDLK_LEFT) {
+				printf("%d", 1);
+			}
+			if (MainEvent.type == SDL_QUIT) {
+				SDL_Quit();
+				break;
+			}
+			else if (MainEvent.type == SDL_MOUSEBUTTONDOWN) {
+				int x = MainEvent.button.x;
+				int y = MainEvent.button.y;
+				for (int i = 0; i < NUM_TARGETS; i++) {
+					if (x >= targets[i].dest.x && x <= targets[i].dest.x + targets[i].dest.w && y >= targets[i].dest.y && y <= targets[i].dest.y + targets[i].dest.h) {
+						targets[i].is_dragging = 1;
 					}
-					break;
-				case SDL_QUIT:
-					QUIT();
-					break;
-				default:
-					break;
 				}
 			}
+			else if (MainEvent.type == SDL_MOUSEBUTTONUP) {
+				for (int i = 0; i < NUM_TARGETS; i++) {
+					targets[i].is_dragging = 0;
+					if (targets[i].dest.x > RB_Rect.x && targets[i].dest.x<RB_Rect.x + RB_Rect.w && targets[i].dest.y>RB_Rect.y && targets[i].dest.y < RB_Rect.y + RB_Rect.h) {
+						targets[i].disappear = 1;
+					}
+				}
+			}
+			else if (MainEvent.type == SDL_MOUSEMOTION) {
+				int dx = MainEvent.motion.xrel;
+				int dy = MainEvent.motion.yrel;
+				for (int i = 0; i < NUM_TARGETS; i++) {
+					if (targets[i].is_dragging) {
+						targets[i].dest.x += dx;
+						targets[i].dest.y += dy;
+					}
+				}
+			}
+			SDL_RenderClear(Renderer);
+			//绘制背景和垃圾
+			SDL_RenderCopy(Renderer, Room_Texture, NULL, &Room_Rect);
+			for (int i = 0; i < NUM_TARGETS; i++) {
+				if (targets[i].disappear==0) {
+					if (targets[i].tartype == 1) {
+						SDL_RenderCopy(Renderer, Banana_Texture, NULL, &targets[i].dest);
+					}
+					else {
+						SDL_RenderCopy(Renderer, Paper_Texture, NULL, &targets[i].dest);
+					}
+				}
+			}
+			SDL_RenderPresent(Renderer);
 			long End = SDL_GetTicks();
 			long Cost = End - Begin;
 			long Frame = 1000 / 60;
@@ -94,139 +169,52 @@ int SDL_main(int argc, char* argv[]) {
 			if (Delay > 0) {
 				SDL_Delay(Delay);
 			}
-		}
-	}
-
-
-
-	return 0;
-}
-
-void LOADMain() {
-	MainSurface = IMG_Load("R.jpg");
-	MainTexture = SDL_CreateTextureFromSurface(Renderer, MainSurface);
-	MainRect.x = 0;
-	MainRect.y = 0;
-	MainRect.w = 710;
-	MainRect.h = 710;
-	SDL_RenderClear(Renderer);
-	SDL_RenderCopy(Renderer, MainTexture, NULL, &MainRect);
-	SDL_RenderPresent(Renderer);
-}
-
-void LOADTitle() {
-	TitleFont = TTF_OpenFont("OPlusSans3-Bold.ttf", 50);
-	TitleSurface = TTF_RenderUTF8_Blended(TitleFont, "Digital Pet", Titlecolor);
-	TitleTexture = SDL_CreateTextureFromSurface(Renderer, TitleSurface);
-	TitleRect.x = 160;
-	TitleRect.y = 18;
-	TitleRect.w = 370;
-	TitleRect.h = 96;
-	SDL_RenderCopy(Renderer, TitleTexture, NULL, &TitleRect);
-	SDL_RenderPresent(Renderer);
-}
-
-void LOADRoom() {
-	StartSurface = IMG_Load("room1.png");
-	StartTexture = SDL_CreateTextureFromSurface(Renderer, StartSurface);
-	StartRect.x = 0;
-	StartRect.y = 0;
-	StartRect.w = 710;
-	StartRect.h = 710;
-	SDL_RenderCopy(Renderer, StartTexture, NULL, &StartRect);
-}
-//随机生成两个垃圾分别把位置储存在rubbishrect中
-void LOADRubbish() {
-	srand(time(NULL));
-	int bop, i;
-	for (i = 0; i < 2; i++) {
-		bop = 1 + (int)(10.0 * rand() / (RAND_MAX + 1.0));
-		if (bop % 2 == 1) {
-			Rubbish_Surface = IMG_Load("banana.png");
-			Rubbish_Texture = SDL_CreateTextureFromSurface(Renderer, Rubbish_Surface);
-			Rubbishrect[i].x = Rubbish_Rect.x = (int)(401 * rand() / (RAND_MAX + 1.0));
-			Rubbishrect[i].y = Rubbish_Rect.y = 500 + (int)(181 * rand() / (RAND_MAX + 1.0));
-			Rubbishrect[i].w = Rubbish_Rect.w = 60;
-			Rubbishrect[i].h = Rubbish_Rect.h = 60;
-			rtype[i] = 1;
-			SDL_RenderCopy(Renderer, Rubbish_Texture, NULL, &Rubbish_Rect);
-		}
-		else {
-			Rubbish_Surface = IMG_Load("paper.png");
-			Rubbish_Texture = SDL_CreateTextureFromSurface(Renderer, Rubbish_Surface);
-			Rubbishrect[i].x = Rubbish_Rect.x = (int)(401 * rand() / (RAND_MAX + 1.0));
-			Rubbishrect[i].y = Rubbish_Rect.y = 500 + (int)(181 * rand() / (RAND_MAX + 1.0));
-			Rubbishrect[i].w = Rubbish_Rect.w = 60;
-			Rubbishrect[i].h = Rubbish_Rect.h = 60;
-			rtype[i] = 2;
-			SDL_RenderCopy(Renderer, Rubbish_Texture, NULL, &Rubbish_Rect);
-		}
-	}
-}
-//把两个垃圾和背景呈现在屏幕上
-void DrawRubbish() {
-	LOADRoom();
-	int i;
-	for (i = 0; i < 2; i++) {
-		Rubbish_Rect.x = Rubbishrect[i].x;
-		Rubbish_Rect.y = Rubbishrect[i].y;
-		Rubbish_Rect.w = Rubbishrect[i].w;
-		Rubbish_Rect.h = Rubbishrect[i].h;
-		if (rtype[i] == 1) {
-			Rubbish_Surface = IMG_Load("banana.png");
-			Rubbish_Texture = SDL_CreateTextureFromSurface(Renderer, Rubbish_Surface);
-		}
-		else {
-			Rubbish_Surface = IMG_Load("paper.png");
-			Rubbish_Texture = SDL_CreateTextureFromSurface(Renderer, Rubbish_Surface);
-		}
-		SDL_RenderCopy(Renderer, Rubbish_Texture, NULL, &Rubbish_Rect);
-	}
-	SDL_RenderPresent(Renderer);
-}
-//b站陈云老师视频里将鼠标指令封装成函数，方便对多个对象执行
-void RectShapeonMouseEvent(SDL_Event *event,int i) {
-	switch (event->type) {
-	case SDL_MOUSEMOTION:
-		if (rDragenable == 1) {
-			//for (i = 0; i < 2; i++) {
-			Rubbishrect[i].x = rDragStartPoint.x + event->motion.x - rDragStartMousePoint.x;
-			Rubbishrect[i].y = rDragStartPoint.y + event->motion.y - rDragStartMousePoint.y;
-			DrawRubbish();
-			//}		
-		}
-		break;
-	case SDL_MOUSEBUTTONDOWN:
-		currentMousePoint.x = event->button.x;
-		currentMousePoint.y = event->button.y;
-		if (SDL_PointInRect(&currentMousePoint, &Rubbishrect[i])) {
-			rDragStartPoint.x = Rubbishrect[i].x;
-			rDragStartPoint.y = Rubbishrect[i].y;
-			rDragStartMousePoint.x = event->button.x;
-			rDragStartMousePoint.y = event->button.y;
-			rDragenable = 1;
+			for (int i = 0; i < NUM_TARGETS; i++) {
+				count = count + targets[i].disappear;
 			}
-		break;
-	case SDL_MOUSEBUTTONUP:
-		rDragenable = 0;
-		break;
-	case SDL_QUIT:
-		QUIT();
-		break;
-	default:
-		break;
+			if (count == 3) {
+				Enternextscene = 0;			}
+			else {
+				count = 0;
+			}
+		}
 	}
 }
+void event_loop2() {
+	walkspeed = 10;
+	bg_x = 0;
+	while (1) {
+		SDL_Event MainEvent;
+		long Begin = SDL_GetTicks;
+		SDL_Rect src1 = { bg_x, 0, 710, 710 };          // 捕获图片区域1
+		SDL_Rect src2 = { 0, 0, bg_x, 710 };               // 捕获图片区域2
+		SDL_Rect put1 = { 0, 0, 710 - bg_x, 710 };      // 显示区域1
+		SDL_Rect put2 = { 710-bg_x, 0, 710, 710 }; // 显示区域2
 
-void QUIT() {
-	SDL_FreeSurface(StartSurface);
-	SDL_FreeSurface(Rubbish_Surface);
-	SDL_DestroyTexture(MainTexture);
-	SDL_DestroyTexture(TitleTexture);
-	SDL_DestroyTexture(StartTexture);
-	SDL_DestroyTexture(Rubbish_Texture);
-	TTF_CloseFont(TitleFont);
-	SDL_DestroyWindow(Window);
-	SDL_DestroyRenderer(Renderer);
-	SDL_Quit();
+		if (SDL_PollEvent(&MainEvent)){
+			if (MainEvent.type == SDL_QUIT) {
+				SDL_Quit();
+				return;
+			}
+			if (MainEvent.key.keysym.sym == SDLK_LEFT) {
+				bg_x = bg_x - walkspeed;
+				printf("%d", bg_x);
+			}
+			if (MainEvent.key.keysym.sym == SDLK_RIGHT) {
+				bg_x = bg_x + walkspeed;
+				printf("%d", bg_x);
+			}
+		}
+		if (bg_x > 710) {
+			bg_x = 0;
+		}
+		if (bg_x < 0) {
+			bg_x = 710;
+		}
+		//这里开始写绘制场地和人物
+		SDL_RenderClear(Renderer);
+		SDL_RenderCopy(Renderer, BG_Texture, &src1, &put1);
+		SDL_RenderCopy(Renderer, BG_Texture, &src2, &put2);
+		SDL_RenderPresent(Renderer);
+	}
 }
